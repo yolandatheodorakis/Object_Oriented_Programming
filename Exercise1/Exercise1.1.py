834537b3c14de64fe9f0c318374c3f34c15a4314
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-11 14:34:37 +0200

    Prints Hello

---
+++ Exercise1/Exercise1.1.py
@@ -0,0 +1,7 @@
+# File name: Exercise1.1.py
+# Author: Yolanda Theodorakis
+# Description: Write a code that prints out 'Hello' in order to verify that 
+#              your development environment is properly set up.
+
+# Print Hello
+print('Hello')
