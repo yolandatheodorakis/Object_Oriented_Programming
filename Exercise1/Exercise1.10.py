bc4475705308e0d5aae30dcf7a8014ac7b355fc7
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-17 13:50:55 +0200

    Term explanations

---
+++ Exercise1/Exercise1.10.py
@@ -0,0 +1,40 @@
+# File name: Exercise1.10.py
+# Author: Yolanda Theodorakis
+# Description: Explain the following terms: 
+#              a.Procedural programming 
+#              b.Functional programming
+#              c.Object oriented programming 
+#              d.Class (in programming) 
+#              e.Object (in programming) 
+#              f.Instance (in programming) 
+#              g.Encapsulation (in programming)
+
+# a. Procedural programming is a programming paradigm. Procedural programming's 
+#    focus is to break a programming task down to procedures. Procedures can 
+#    be called at any point during a program's execution.
+
+# b. Functional programming is a programming paradigm. In functional 
+#    programming function definitions are trees of expressions that each return 
+#    a value which allows programs to be written in a declarative and 
+#    composable style.
+
+# c. Object oriented programming is a programming paradigm. OOP is based on 
+#    objects which contain properties and methods. Object's own procedures 
+#    can access and modify the data fields of itself.
+
+# d. Classes are used in object oriented programming. Class is like a template 
+#    for creating objects. It provides initial values for state and 
+#    implementations of behavior.
+
+# e. Object can be a variable, a data structure, a function, or a method or in 
+#    object oriented programming a combination of those. In relational databases 
+#    object can be a table or a column.
+
+# f. Instance can be synonymous with object in object oriented programming. 
+#    Instance is a concrete occurrence of any object, existing usually 
+#    during the runtime of a computer program.
+
+# g. Encapsulation means the bundling of data with the methods that operate on 
+#    that data, or the restricting of direct access to some of an object's 
+#    components. It's used in object oriented programming to hide the values 
+#    or state of a structured data object inside a class.
