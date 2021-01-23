5a0a63cb6d07068eb5837b3d33f2a963f160bbd0
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-23 20:52:34 +0200

    Pseudocode for the grade average

---
+++ Exercise2/Exercise2.4.py
@@ -0,0 +1,17 @@
+# File name: Exercise2.4.py
+# Author: Yolanda Theodorakis
+# Description: Write pseudocode for a program that accepts student’s name and 
+#              grade as input and counts the average of grades of all students. 
+#              If you have difficulties, you can fix the number of students to 
+#              e.g. 5. Print out the average. Use informative and readable 
+#              output prints.
+
+#   function count_average:
+#        while the number of students is under 6:
+#            ask the user to input name and grade
+
+#            count the sum of all grades into a variable
+
+#            count the average and save into a variable
+
+#       print what the average is
