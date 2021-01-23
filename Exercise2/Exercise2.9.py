8c36b99830a5f508e39d06baba01986d33020488
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-23 20:54:36 +0200

    Pseudocode for the alarm clock

---
+++ Exercise2/Exercise2.9.py
@@ -0,0 +1,40 @@
+# File name: Exercise2.9.py
+# Author: Yolanda Theodorakis
+# Description: Write pseudocode for the alarm clock.
+
+#   class AlarmClock:
+
+#       initialize the states:
+#           current time
+#           hours for the alarm = 0
+#           minutes for the alarm = 0
+#           seconds for the alarm = 0
+#           alarm
+
+#       get current time:
+#           return current time
+
+#       set alarm hours:
+#           ask user to input hours
+#       set alarm minutes:
+#           ask user to input minutes
+#       set alarm seconds:
+#           ask user to input seconds
+
+#       set alarm:
+#           compile the inputs into one
+
+#       get alarm:
+#       return alarm
+
+
+#   main function:
+#       create new object from the class
+
+#       print current time
+
+#       ask user to set the alarm hours, minutes and seconds
+
+#       set the alarm
+
+#       print what the alarm is
