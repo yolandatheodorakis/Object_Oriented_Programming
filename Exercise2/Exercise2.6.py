987acb78466db9ce302ad78014a162090012afc2
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-23 20:53:19 +0200

    Planning for the alarm clock

---
+++ Exercise2/Exercise2.6.py
@@ -0,0 +1,13 @@
+# File name: Exercise2.6.py
+# Author: Yolanda Theodorakis
+# Description: Imagine you would have to code a simple alarm clock (shows time 
+#              and alarms you at certain time you can set). Which data 
+#              attributes will you have? Do the attributes have some value 
+#              restrictions? You should find at least 5 data attributes. Which 
+#              methods would you need? Which methods should be public and 
+#              which ones should be private?
+
+# Attributes: hours 0-24, minutes 0-60, seconds 0-60, alarm_time, 
+#             on_off for each alarm
+# Methods: set_alarm_hours, set_alarm_minutes, set_alarm_seconds, 
+#          set_alarm on/off, show_time, these all are public
