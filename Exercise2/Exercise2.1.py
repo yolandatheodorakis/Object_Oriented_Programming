bc91660d59261d1adf1b3464419a4f40b3a1a8a7
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-23 20:48:46 +0200

    Explaining the terms

---
+++ Exercise2/Exercise2.1.py
@@ -0,0 +1,21 @@
+# File name: Exercise2.1.py
+# Author: Yolanda Theodorakis
+# Description: Explain the following terms: 
+#              a. Pseudocode
+#              b. Algorithm
+#              c. Data attribute
+#              d. Method
+
+# a. Pseudocode means code written in plain language rather than any coding 
+#    language. It has no syntax so computers can't read it but any person can 
+#    without much coding knowledge.
+
+# b. An algorithm uses a set of rules or instructions to solve a problem or 
+#    to perform a computation.
+
+# c. Data attribute stores custom data, for example every object has 
+#    attributes like name, age etc.
+
+# d. Method is similar to a function. It is a subroutine attached to a 
+#    specific class and it can only be called by an object created from 
+#    the class.
