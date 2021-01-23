66fa537ef296b5e2ff788c91ced5780691188274
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-23 20:49:22 +0200

    Pseudocode for the grade average

---
+++ Exercise2/Exercise2.2.py
@@ -0,0 +1,28 @@
+# File name: Exercise2.2.py
+# Author: Yolanda Theodorakis
+# Description: Take a look at the course’s assessment (number of accepted 
+#              exercises meaning certain grade). Write pseudocode for a 
+#              program where user inputs the number of accepted exercises and 
+#              program prints out the grade. Use informative and readable 
+#              output prints.
+
+#   function get_grade:
+#       ask user to input the number of accepted exercises
+
+#       if user's input is smaller than 9:
+#           print: grade is 0
+
+#       if user's input is 9:
+#           print: grade is 1
+
+#       if user's input is 10:
+#           print: grade is 2
+
+#       if user's input is 11:
+#           print: grade is 3
+
+#       if user's input is 12:
+#           print: grade is 4
+
+#       if user's input is 13:
+#           print: grade is 5
