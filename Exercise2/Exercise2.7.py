6afd434a490fc07f88b6958d18a0984a3e095a1f
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-23 20:53:39 +0200

    Coin toss

---
+++ Exercise2/Exercise2.7.py
@@ -0,0 +1,47 @@
+# File name: Exercise2.7.py
+# Author: Yolanda Theodorakis
+# Description: Take a look at the coin.py, write it down in your IDE and run 
+#              it. See that coin gets tossed.
+
+import random
+
+class Coin:
+
+    # The __init__ method initializes the sideup data attribute with 'Heads'
+    def __init__ (self):
+        self.sideup = 'Heads'
+
+    # The toss method generates a random number in the range of 0 through 1 
+    # If the number is 0, then sideup is set to 'Heads',
+    # otherwise, sideup is set to 'Tails'
+    def toss (self):
+        if random.randint(0, 1)  == 0:
+            self.sideup = 'Heads'
+
+        else:
+            self.sideup = 'Tails'
+
+    # The get_sideup method returns the value referenced by sideup
+    def get_sideup (self):
+        return self.sideup
+
+
+# The main function
+def main ():
+
+    # Create an object from the Coin class
+    my_coin = Coin()
+
+    # Display the side of the coin that is facing up
+    print('This side is up: ', my_coin.get_sideup())
+
+    # Toss the coin
+    print('I am tossing the coin ...')
+    my_coin.toss()
+
+    # Display the side of the coin that is now facing up
+    print('This side is up: ', my_coin.get_sideup())
+
+
+# Call the main function
+main()
