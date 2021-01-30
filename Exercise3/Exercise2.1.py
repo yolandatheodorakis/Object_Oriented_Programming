fb8898b26eb97a5864484f610217952b0f38a177
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-01-30 13:54:21 +0200

    Explaining the terms

---
+++ Exercise3/Exercise2.1.py
@@ -0,0 +1,25 @@
+# File name: Exercise3.1.py
+# Author: Yolanda Theodorakis
+# Description: Explain the following terms:
+#              a. Abstraction (in programming)
+#              b. Accessor and mutator methods
+#              c. Public and private methods
+#              d. __str__ method (in Python)
+
+# a. Abstraction means hiding all except the relevant data of an object, 
+#    whenever practical, from the user. This reduces complexity and 
+#    increases efficiency.
+
+# b. Accessor and mutator methods help access and alter internally stored 
+#    data. Accessors (get methods) are used to access the data hidden in the 
+#    obejct. They can't be used to change the data.
+#    Mutators (set methods) are used to modify the hidden data.
+
+# c. Private methods are used to hide the inner functionality of any class. 
+#    Private methods should not be accessed outside the class or by any base 
+#    class. Private methods are defined with "__" prefix.
+#    Public methods are accessible from outside the class.
+
+# d. The __str__ method represents the class ojects as a string. It is usually 
+#    used for classes. It should be defined so that it's easy to read and it 
+#    outputs all the members of the class.
