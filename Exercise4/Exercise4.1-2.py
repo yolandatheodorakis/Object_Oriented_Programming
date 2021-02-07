082d5e816c8602a118f7ee6e1ab05c9a118847ed
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-02-07 02:02:16 +0200

    Explaining the terms

---
+++ Exercise4/Exercise4.1-2.py
@@ -0,0 +1,31 @@
+# File name: Exercise4.1-2.py
+# Author: Yolanda Theodorakis
+# Description: 1. Explain the following terms and what they are used for:
+#                 a. Inheritance (in object-oriented programming)
+#                 b. UML
+#                 c. UML class diagram
+#              2. When you model using UML diagrams, why is it important to 
+#                 follow the UML syntax strictly?
+
+# a. One class inheriting the attributes and methods of another class is 
+#    called inheritance. A class that inherits from a parent class is called 
+#    a subclass or child class, and objects that receive properties from a 
+#    parent through inheritance are referred to as child objects. Inheritance 
+#    allows the creation of classes that are built upon existing classes, to 
+#    specify a new implementation while maintaining the same behaviors, to 
+#    reuse code and to independently extend original software via public 
+#    classes and interfaces.
+
+# b. UML refers to Unified Modeling Language. It is a standardized modeling 
+#    language consisting of an integrated set of diagrams. UML is intended to 
+#    provide a standard way to visualize the design of a system.
+
+# c. A class diagram in UML is the main building block of object-oriented 
+#    modeling. It is a type of static structure diagram that describes the 
+#    structure of a system by showing the system's classes, their attributes, 
+#    methods, and the relationships among objects. The class diagram is used 
+#    for general conceptual modeling of the structure of the application, and 
+#    for detailed modeling translating the models into programming code.
+
+# 2. The UML syntax is the most important thing in modeling. The model is 
+#    useless, unless its purpose is depicted properly with syntax.
