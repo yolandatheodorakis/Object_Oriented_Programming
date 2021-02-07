d2e943a25ceb75d3cd2e709676cc54b1a71ca467
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-02-07 03:14:58 +0200

    Added the id attribute

--- Exercise4/Exercise4CellPhoneClass.py
+++ Exercise4/Exercise4CellPhoneClass.py
@@ -3,13 +3,18 @@
 # Description: Take the Cell phone class of last week and divide the cell 
 #              phone class into another file. Leave the main function in the 
 #              original file. Test that your code still works.
+#              Add and ID data attribute (integer between 1-6) to the cell 
+#              phone. Cell phone class shall have accessor and mutator methods 
+#              for all data attributes. Also check the __str__ method is up 
+#              to date.
 
 class CellPhone:
-    # Initialize the states for manufacturer, model and retail price
+    # Initialize the states for manufacturer, model, retail price and id
     def __init__ (self):
         self.manufact = ''
         self.model = ''
         self.retail_price = ''
+        self.id = 1
 
     # Set the manufacturer from user input
     def set_manufact (self):
@@ -22,6 +27,10 @@ class CellPhone:
     # Set the retail price from user input
     def set_retail_price (self):
         self.retail_price = input('Enter the retail price: ')
+
+    # Set the id from user input
+    def set_id (self):
+        self.id = input('Enter the id: ')
     
     # Return the manufacturer
     def get_manufact (self):
@@ -35,8 +44,13 @@ class CellPhone:
     def get_retail_price (self):
         return self.retail_price
 
+    # Return the id
+    def get_id (self):
+        return self.id
+
     # Print the state
     def __str__ (self):
         return ('Manufacturer: ' + format(self.manufact) +
                 '\nModel numeber: ' + format(self.model) + 
-                '\nRetail price: ' + format(self.retail_price))
+                '\nRetail price: ' + format(self.retail_price) + 
+                '\nID: ' + format(self.id))
--- Exercise4/Exercise4CellPhoneMain.py
+++ Exercise4/Exercise4CellPhoneMain.py
@@ -3,6 +3,10 @@
 # Description: Take the Cell phone class of last week and divide the cell 
 #              phone class into another file. Leave the main function in the 
 #              original file. Test that your code still works.
+#              Add and ID data attribute (integer between 1-6) to the cell 
+#              phone. Cell phone class shall have accessor and mutator methods 
+#              for all data attributes. Also check the __str__ method is up 
+#              to date.
 
 import Exercise4CellPhoneClass as phone
 
@@ -10,10 +14,11 @@ def main ():
     # Create new object from CellPhone class
     cell_phone = phone.CellPhone()
 
-    # Ask for user to input manufacturer, model and retail price
+    # Ask for user to input manufacturer, model, retail price and id
     cell_phone.set_manufact()
     cell_phone.set_model()
     cell_phone.set_retail_price()
+    cell_phone.set_id()
 
     # Print the data
     print('Here is the data that you provided:')
