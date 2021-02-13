026be8de92ff5ab367a0c20e9393a13d5d2b0ce7
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-02-13 20:34:38 +0200

    Tasks 1-3

---
+++ Exercise5/Exercise5.1-3.py
@@ -0,0 +1,37 @@
+# File name: Exercise5.1-3.py
+# Author: Yolanda Theodorakis
+# Description: Tasks 1-3
+
+# 1. Explain the following term and what is it used for: a. Multiple inheritance
+#    A class that is derived from more than one base class is called multiple 
+#    inheritance. All the features of the base classes are inherited to the 
+#    derived class. Multiple inheritance is used to add functionality that 
+#    might not be part of the logical class relationships.
+
+# 2. True or false?
+#    a. The practice of procedural programming is centered on the creation 
+#       of objects. FALSE
+#    b. Object reusability has been a factor in the increased use of 
+#       object-oriented programming. TRUE
+#    c. It is a common practice in object-oriented programming to make all of 
+#       a class’s data attributes accessible to statements outside the class. FALSE
+#    d. A class methods does not have to have a self parameter. FALSE
+#    e. Starting an attribute name with two underscores will hide the attribute 
+#       from code outside the class. TRUE
+#    f. You cannot directly call the __str__method. TRUE
+
+# 3. Multiple choice:
+#    a. The ___ method is automatically called when an object is created.
+#       i. __init__
+#    b. The ___ programming practice is centered on creating functions that are 
+#       separated from the data that they work on.
+#       ii. procedural
+#    c. The ___ programming practice iscentered on creating objects.
+#       iv. object-oriented
+#    d. A(n) ___ is a component of a class that references data.
+#       iii. data attribute
+#    e. By doing this, you can hide a class's attribute from code outside the class.
+#       iii. begin the name of the attribute with two underscores
+#    f. A(n) ___ method stores a value in the data attribute or changes its value 
+#       in some way.
+#       iii. mutator
