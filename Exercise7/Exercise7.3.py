4b0ccb21beb400d8be1bab3e65aca232b96c8ee5
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-03-06 19:12:16 +0200

    True or false

---
+++ Exercise7/Exercise7.3.py
@@ -0,0 +1,27 @@
+# File name: Exercise7.3.py
+# Author: Yolanda Theodorakis
+# Description: True or false?
+
+# a. It is not possible to call a superclass’s __init__ method from a 
+#    subclass’s __init__ method.
+#    FALSE
+
+# b. A subclass never inherits any methods or attributes from the superclass. 
+#    FALSE
+
+# c. A superclass can inherit methods from subclass, if they have been denoted 
+#    with pass_to_superfunction. 
+#    TRUE
+
+# d. In a subclass it is possible to have methods and attributes in addition to 
+#    those that the subclass inherits from superclass. 
+#    TRUE
+
+# e. In Python, multiple inheritance does not exist. 
+#    FALSE
+
+# f. Aggregation and composition shall never be used in UML class diagrams. 
+#    FALSE
+
+# g. Aggregation and composition mean exactly the same thing in UML class diagrams.
+#    FALSE
