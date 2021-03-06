e23d531b4bf236312995f1f22c30ab9238980247
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-03-06 18:59:08 +0200

    Multiple choice questions

---
+++ Exercise7/Exercise7.2.py
@@ -0,0 +1,26 @@
+# File name: Exercise7.2.py
+# Author: Yolanda Theodorakis
+# Description: Multiple choice questions:
+
+# a. In an inheritance relationship, the ___ is the general class.
+#    iii. Superclass
+
+# b. In an inheritance relationship, the ___ is the specialized class.
+#    iv. Subclass
+
+# c. Let’s say we have two classes in our program: BankAccount and SavingsAccount. 
+#    Which one of them would most likely be the subclass?
+#    ii. SavingsAccount
+
+# d. Which one of the options you will use if you want to check whether an object 
+#    is an instance of a class?
+#    iii. The isinstance function
+
+# e. Which one of the UML diagrams is a behavioral diagram?
+#    ii. Sequence diagram
+
+# f. Which one of the UML diagrams is a structural diagram?
+#    iv. Composite structure diagram
+
+# g. In UML class diagrams, what does the notation * mean?
+#    iii. Multiplicity 0..n
