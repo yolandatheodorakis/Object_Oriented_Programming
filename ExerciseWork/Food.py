9e235add5cfae9d52cf7332f85c7495cd54c1bd5
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-03-28 02:54:31 +0200

    Start of Food class

---
+++ ExerciseWork/Food.py
@@ -0,0 +1,9 @@
+class Food:
+    # Initialize the states
+    def __init__(self):
+        self.x_position = 250
+        self.y_position = 125
+        self.width = 10
+        self.height = 10
+        self.color = (255, 153, 255)
+        
