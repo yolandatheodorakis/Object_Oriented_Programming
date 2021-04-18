b31d81d5ca32c6628c7b45fcfebc231676dfa892
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-04-18 15:14:19 +0300

    The game functions

--- ExerciseWork/Game.py
+++ ExerciseWork/Game.py
@@ -1,14 +1,14 @@
-# File  name: Game.py
+# File name: Game.py
 # Author: Yolanda Theodorakis
 # Description: Snake game
 
 import pygame as pg
+import time
 import Snake as snake
 import Food as food
 
-# Variables for the game display
-screen_width = 500
-screen_height = 500
+# Variable for the game display
+window = pg.display.set_mode((700, 700))
 
 # Colors
 black = pg.Color(0, 0, 0)
@@ -17,37 +17,50 @@ white = pg.Color(255, 255, 255)
 # Variables for scrores
 score, high_score = (0, 0)
 
-# Draws the score on the screen
+
+# Displays the score on the screen
 def show_score(surface):
     global high_score
     font_name = pg.font.match_font('arial')
+
+    # Update the highest score
     if score > high_score:
         high_score = score
+
     # Writing the score
     font = pg.font.Font(font_name, 18)
-    text_surface = font.render('Score: {} High Score: {}'.format(score, high_score), True, white)
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (200, 10)
-    surface.blit(text_surface, text_rect)
+    text1_surface = font.render('Score: {}'.format(score), True, white)
+    text1_rect = text1_surface.get_rect()
+    text1_rect.midtop = (350, 10)
+    surface.blit(text1_surface, text1_rect)
+    text2_surface = font.render('High Score: {}'.format(high_score), True, white)
+    text2_rect = text2_surface.get_rect()
+    text2_rect.midtop = (350, 30)
+    surface.blit(text2_surface, text2_rect)
+
 
 # What to do when the game is over
 def game_over():
     global score
+
     # Write 'game over' to the screen
     game_over_font = pg.font.Font('freesansbold.ttf', 24)
-    game_over_surface = game_over_font.render('Game Over', True, white)
+    game_over_surface = game_over_font.render('Game Over :(', True, white)
     game_over_rect = game_over_surface.get_rect()
-    game_over_rect.midtop = (200, 50)
+    game_over_rect.midtop = (350, 300)
     window.blit(game_over_surface, game_over_rect)
+
     # Reset score
     score = 0
     pg.display.flip()
-    time.sleep(2)
+    time.sleep(3)
+
     # Re-initialize game
     run = True
     fd = food.Food()
     s = snake.Snake()
     play_game(fd, s)
+
 
 # The game
 def play_game(food_object, snake_object):
@@ -57,16 +70,19 @@ def play_game(food_object, snake_object):
         # FPS
         clock = pg.time.Clock()
         clock.tick(30)
+
         # Catches all the events that have happened since the game started
         for event in pg.event.get():
             # When you press the 'close window' button
             if event.type == pg.QUIT:
                 run = False
-        # Draw food and snake to the screen and show score
+
+        # Display the score, snake, and food
         window.fill(black)
         food_object.draw_food(window)
         snake_object.draw_snake(window)
         show_score(window)
+
         # User input
         pressed = pg.key.get_pressed()
         if pressed[pg.K_UP]:
@@ -77,28 +93,35 @@ def play_game(food_object, snake_object):
             snake_object.change_direction('down')
         if pressed[pg.K_RIGHT]:
             snake_object.change_direction('right')
+
         # Move the snake
         snake_object.move()
+
         # Eat
         if food_object.is_eaten(snake_object.head):
             food_object.new_position()
             snake_object.increase_length()
             score += 10
-        # Collision
+        # Check for collisions
         if snake_object.is_collision():
             run = False
             game_over()
+
+        # Update the game display
         pg.display.update()
 
-# Initialize pygame
-pg.init()
 
-# The screen
-window = pg.display.set_mode((screen_width, screen_height))
-pg.display.set_caption('A game of Snake')
-fd = food.Food()
-s = snake.Snake()
-play_game(fd, s)
+# The main function
+def main():
+    # Initialize pygame
+    pg.init()
 
-# Exits pygame
-pg.quit()
+    pg.display.set_caption('A game of Snake')
+    fd = food.Food()
+    s = snake.Snake()
+    play_game(fd, s)
+
+    # Exits pygame
+    pg.quit()
+
+main()
